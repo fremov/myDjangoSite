78b266edff469fd07eba268fe568d59296bb81b5
fremov <59342062+fremov@users.noreply.github.com>
2025-01-08 02:18:18 +0300

    first commit

---
+++ bboard/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+from .models import Bb
+
+# Create your views here.
+
+
+def index(request):
+    template = loader.get_template('bboard\index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs' : bbs}
+    return HttpResponse(template.render(context, request))
