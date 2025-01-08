2a4b75de2aa968d5d6d34f065cc4a54e62b50af6
fremov <59342062+fremov@users.noreply.github.com>
2025-01-08 13:58:52 +0300

    added admin panel

--- bboard/views.py
+++ bboard/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-
 from .models import Bb
 
 # Create your views here.
 
-
 def index(request):
-    template = loader.get_template('bboard\index.html')
-    bbs = Bb.objects.order_by('-published')
-    context = {'bbs' : bbs}
-    return HttpResponse(template.render(context, request))
+    bbs = Bb.objects.all()
+    # bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {"bbs": bbs})
